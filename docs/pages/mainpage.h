4de5f2bfb8fefd69946fa708cf2e4670bda47fb7
Josh Bialkowski <jbialk@mit.edu>
2011-08-22 17:10:31 -0400

    Added mainpage

---
+++ docs/pages/mainpage.h
@@ -0,0 +1,21 @@
+/**
+ *  \file   mainpage.h
+ *
+ *  \date   Aug 22, 2011
+ *  \author Josh Bialkowski (jbialk@mit.edu)
+ *  \brief  
+ *
+ *  \mainpage Compiz Momentum Windows (throw) Plugin
+ *
+ *  First install the cmake modules from the git core
+ *  \verbatim
+
+    git clone git://anongit.compiz-fusion.org/compiz/core compiz
+    cd compiz
+    mkdir build
+    cd build
+    cmake ../
+    make findcompiz_install
+\endverbatim
+ *
+ */
